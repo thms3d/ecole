ce5f1b9136928d87b25c0569ce16f412d42bbb7b
thms3d <139813428+thms3d@users.noreply.github.com>
2023-07-27 17:31:30 +0200

    c03 ex03.c

---
+++ c03 ex03.c
@@ -0,0 +1,17 @@
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int				i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j] && j > nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
