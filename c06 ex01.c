c20557bb5faa2a5457bfa767670b63683a771829
thms3d <139813428+thms3d@users.noreply.github.com>
2023-08-02 19:30:55 +0200

    c06 ex01.c

---
+++ c06 ex01.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc > 0)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			write(1, argv[i], ft_strlen(argv[i]));
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
